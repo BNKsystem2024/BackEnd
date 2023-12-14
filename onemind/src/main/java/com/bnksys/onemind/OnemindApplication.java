f51c2a5e4e47efd90e2f5a795418f7df04ebe4f1
김도균 <kimdogyun@gimdogyun-ui-MacBookAir.local>
2023-12-14 14:34:32 +0900

    first commit

---
+++ onemind/src/main/java/com/bnksys/onemind/OnemindApplication.java
@@ -0,0 +1,13 @@
+package com.bnksys.onemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemindApplication.class, args);
+	}
+
+}
