a8376054f685f835ffd61a8013fd5197a2e4ad63
김규리 <mandarining0918@gmail.com>
2023-12-14 15:09:20 +0900

    Feat: init Files

---
+++ onemind/src/main/java/com/bnksys/onemind/OnemindApplication.java
@@ -0,0 +1,12 @@
+package com.bnksys.onemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemindApplication.class, args);
+	}
+}
