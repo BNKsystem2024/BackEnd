73f392e41eb6cfb358009d148874321f0d46e1bf
김규리 <mandarining0918@gmail.com>
2023-12-19 11:20:24 +0900

    Fix: fix JsonProperty name

--- onemind/src/main/java/com/bnksys/onemind/apis/dtos/OwnSolvedQuizListResponse.java
+++ onemind/src/main/java/com/bnksys/onemind/apis/dtos/OwnSolvedQuizListResponse.java
@@ -29,7 +29,7 @@ public class OwnSolvedQuizListResponse {
         @JsonProperty("is_correct")
         private Boolean isCorrect;
 
-        @JsonProperty("is_correct")
+        @JsonProperty("level")g
         private Integer level;
     }
 
